0e5b575d63b0544ef300185ee1424a1d60753332
João Rodrigues <71936513+joaorodrigues-ist@users.noreply.github.com>
2020-10-31 10:06:23 +0000

    Add files via upload

--- dgs-server/src/main/java/pt/tecnico/staysafe/dgs/DgsServerImpl.java
+++ dgs-server/src/main/java/pt/tecnico/staysafe/dgs/DgsServerImpl.java
@@ -1,7 +1,17 @@
 package pt.tecnico.staysafe.dgs;
 
 import pt.tecnico.staysafe.dgs.grpc.*;
+import io.grpc.stub.StreamObserver;
 
 public class DgsServerImpl extends DgsServiceGrpc.DgsServiceImplBase{
 	
+	private DgsSystem dgsSystem = new DgsSystem();
+
+	public void ctrlPing(PingRequest request, StreamObserver<PingResponse> responseObserver)
+	{
+		String output = "Dgs server state\n" + dgsSystem.observationsToString();
+		PingResponse response = PingResponse.newBuilder().setResult(output).build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
 }
