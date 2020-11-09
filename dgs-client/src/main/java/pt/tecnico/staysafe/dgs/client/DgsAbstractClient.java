a5ee536121868087c49c2dd1e14c337f3d3981eb
João Rodrigues <71936513+joaorodrigues-ist@users.noreply.github.com>
2020-11-09 22:39:13 +0000

    Added the SnifferAddReportCommand

--- dgs-client/src/main/java/pt/tecnico/staysafe/dgs/client/DgsAbstractClient.java
+++ dgs-client/src/main/java/pt/tecnico/staysafe/dgs/client/DgsAbstractClient.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import pt.tecnico.staysafe.dgs.grpc.*;
+import pt.tecnico.staysafe.sniffer.ReportRequest;
 import io.grpc.StatusRuntimeException;
 
 import com.google.protobuf.Timestamp;
@@ -58,7 +59,7 @@ public abstract class DgsAbstractClient {
 		_commands.add(new ClearCommand(_frontend));
 
 	}
-
+	
 	// read input until blank line or EOF and execute accordingly
 	public final void run() {
 		Scanner scanner = new Scanner(System.in);
@@ -373,4 +374,131 @@ class SnifferInfoCommand extends Command {
 	    SnifferInfoResponse response = _fe.snifferInfo( request );
 		return response.getAddress();
 	}
+}
+
+//for sniffer simulation
+class SnifferAddReportCommand extends Command
+{
+	private String _snifferName;
+	
+	public SnifferAddReportCommand(DgsFrontend fe, String snifferName)
+	{
+		super("sniffer_join_command", 1, 1, fe);
+		_snifferName = snifferName;
+	}
+	
+	private Boolean errorChecking(String [] words)
+	{
+       //check if we have the correct number of words
+		if(!((words.length == 4 && !words[0].equals("sleep")) || (words.length == 2 && words[0].equals("sleep"))))
+			return false;
+     
+		//check if we have valid timestamps
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss");
+
+		try {
+			LocalDateTime dateTime = LocalDateTime.parse(words[2], formatter);
+			dateTime = LocalDateTime.parse(words[3], formatter);
+ 		} catch (DateTimeParseException dtpe) {
+     		return false;
+ 		}
+     
+		//check if we have valid person type and citizen id
+		if(!((words[0].equals("infetado") || words[0].equals("nao-infetado")) && words[1].matches("[0-9]+")))
+			return false;
+ 
+ 		return true;
+	}
+	
+	@Override
+	public String getHelp()
+	{
+		return "sniffer_command - to test sniffer client.";
+	}
+	
+	@Override
+	public String execute(String [] args) throws IOException, StatusRuntimeException {	
+		
+		// buffer of messages to send
+		ArrayList<String[]> buffer = new ArrayList<>();
+ 
+		//variables
+		String [] words;
+		ReportRequest obs;
+		Timestamp entryTimestamp;
+		Timestamp leaveTimestamp;
+	    Date entryDate;
+	    Date leaveDate;
+	    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Integer counter = 0;
+		
+		// main cycle
+		for ( String input: args ) {
+			
+			// check if we are reading a comment
+			if ( input.charAt(0) == '#' ) 
+				continue;
+ 
+			// split input by function name and args
+			words = input.split(",");
+ 
+			//Removing the whitespaces in the beginning and end of the words
+			for(int i = 0; i < words.length; i++)
+				words[i] = words[i].trim();
+ 
+			//check if is to sleep
+			if(words.length == 2)
+			{
+				if(words[1].matches("[0-9]+"))
+				{
+					try
+					{
+						
+						int seconds = Integer.parseInt(words[1]);
+						//for testing only
+						System.out.println("Sleeping for " + seconds + "seconds.");
+						Thread.sleep(seconds);
+					} catch(Exception te)
+					{
+						return "ERROR: thread sleep exception";
+					}
+				}
+				else
+					return "ERROR: Invalid Input!";
+			}
+			
+			//check if valid observation
+			if(errorChecking(words) == false)
+			{
+				return "ERROR: Invalid Input!";
+			}
+			
+			//if everything is ok
+			buffer.add(words);
+         
+		} // end of main cycle
+		
+		for ( String[] observation : buffer ) {
+			// build google Timestamp
+			try {
+		        // create Date objects
+	        	entryDate = format.parse( observation[2] );
+	        	leaveDate = format.parse( observation[3] );
+				
+	        	// adapt to Timestamp objects
+	        	entryTimestamp = Timestamp.newBuilder().setSeconds( entryDate.getTime()/1000L ).buildPartial();
+	          	leaveTimestamp = Timestamp.newBuilder().setSeconds( leaveDate.getTime()/1000L ).buildPartial();
+	        	
+				// build observation object
+				obs = ReportRequest.newBuilder().setSnifferName(_snifferName).setType( observation[0].equals("infetado") ? PersonType.INFECTED : PersonType.NOT_INFECTED).
+						setCitizenId( Integer.parseInt(observation[1]) ).setEnterTime( entryTimestamp  ).setLeaveTime( leaveTimestamp ).
+						build();
+				
+				// send report
+				_fe.report(obs);
+			}
+			catch (Exception exp) {
+				return "ERROR: while building dates in observation.";
+			}
+		}
 }
