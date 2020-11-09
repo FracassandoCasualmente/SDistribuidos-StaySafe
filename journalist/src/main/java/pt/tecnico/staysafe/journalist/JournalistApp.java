32eb1590cc5f7d11a675045d30e1a00f833ed15f
João Rodrigues <71936513+joaorodrigues-ist@users.noreply.github.com>
2020-11-09 14:16:20 +0000

    Add files via upload

--- journalist/src/main/java/pt/tecnico/staysafe/journalist/JournalistApp.java
+++ journalist/src/main/java/pt/tecnico/staysafe/journalist/JournalistApp.java
@@ -1,9 +1,17 @@
 package pt.tecnico.staysafe.journalist;
 
+import pt.tecnico.staysafe.dgs.client.DgsAbstractClient;
 
-public class JournalistApp {
+public class JournalistApp extends DgsAbstractClient{
 	
+	//call the super class constructor because of frontend
+	public JournalistApp(String host, int port)
+	{
+		super(host,port,"journalist");
+	}
+		
 	public static void main(String[] args) {
+						
 		System.out.println(JournalistApp.class.getSimpleName());
 		
 		// receive and print arguments
@@ -11,6 +19,27 @@ public class JournalistApp {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+		
+		//basic arguments checking
+		if(args.length != 2)
+		{
+			System.out.println("ERROR: Invalid number of arguments!");
+			System.out.println("./journalist host port");
+			System.exit(-1);
+		}
+		
+		//Connection parameters
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		
+		JournalistApp journalist = new JournalistApp(host,port);
+			
+		//Add journalist commands
+		journalist.run();
+		journalist.close();
+		
+		 
 	}
+
 
 }
